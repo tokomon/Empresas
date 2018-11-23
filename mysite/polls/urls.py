c7803d16d917c0e158762e74b9af4f2bd825d847
tokomon <ang.18.vc@gmail.com>
2018-11-23 14:18:58 -0500

    Actualizacion

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,14 +5,15 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
  # ex: /polls/
-    path('product/', views.ProductoView.as_view(), name='product'),
+    path('', views.ProductoView.as_view(), name='product'),
 
     path('', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
     path('precio/', views.precio, name='precio'),
+    path('soup/', views.index1.as_view(), name='soup'),
+
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,10 +2,53 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+import json
+import requests
 
-from .models import Choice, Question,Product,Producto
+from .models import Choice, Question,Producto
 from .forms import NameForm
 
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup("<html><p>This is <b>invalid HTML</p></html>", "html.parser")
+
+class index1(generic.ListView):
+    template_name='polls/sop.html'
+    context_object_name = 'product_list'
+    def get_queryset(self):
+        pag = ['https://www.linio.com.pe/c/computacion/portatiles','https://www.linio.com.pe/c/celulares-y-tablets/tablets',
+               'https://www.linio.com.pe/c/tv-audio-y-video/tv-y-video',
+               'https://www.linio.com.pe/c/celulares-y-tablets/celulares-y-smartphones']
+        cat = ['Laptops','Tablets','Televisores','Celulares']
+        for i in range (0,len(pag)):
+            req = requests.get(pag[i])
+            soup = BeautifulSoup(req.text, "html.parser")
+            #soup = BeautifulSoup(req.text, "lxml")
+            list = []
+
+            prod = soup.findAll('div', attrs = {'class': 'catalogue-product row'})
+            for name_box in prod:
+                # data = soup.find('script',attrs = {'type':'text/javascript'})
+                url ="https://www.linio.com.pe"+ name_box.find('a')['href']
+
+                name = name_box.find('meta', attrs={'itemprop': 'name'})['content']
+                model = name_box.find('meta', attrs={'itemprop': 'model'})['content']
+                image ="https:"+ name_box.find('meta', attrs={'itemprop': 'image'})['content']
+                price = name_box.find('div', attrs={'class': 'detail-container'}).find('meta', attrs={'itemprop': 'price'})[
+                    'content']
+                # marca = name_box.find('div' ,attrs={'class':'detail-container'})
+                # some JSON:
+                pdct = {"name": name, "model": model, "image": image, "price": float(price),'url':url}
+                p = Producto(nombre = name, precio=price,url = url ,image = image, modelo = "",
+                             marca ="",direccion = "",categoria =cat[i],tienda = "Linio",url_tienda="https://www.linio.com.pe")
+                p.save()
+                list.append(pdct)
+
+        return Producto.objects.all()
+
+    #return render(request, 'polls/sop.html', {'name': list})
+
+    #HttpResponse(at['content'])
 
 class ProductView(generic.ListView):
     template_name='polls/product.html'
@@ -20,7 +63,7 @@ class ProductoView(generic.ListView):
     context_object_name = 'product_list'
     def get_queryset(self):
         #return Producto.objects.all()
-        return Producto.objects.order_by('price_value')[:5]
+        return Producto.objects.order_by('precio')
 
 def precio(request):
     template_name = 'polls/product.html'
@@ -28,7 +71,7 @@ def precio(request):
 
     def get_queryset(self):
         # return Producto.objects.all()
-        return Producto.objects.order_by('price_value')[:5]
+        return Producto.objects.filter(categoria="Laptops").order_by('price_value')[:5]
 
     return HttpResponse("Hello, world. You're at the polls index.")
 
@@ -50,6 +93,8 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+
 """    
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -65,6 +110,8 @@ def results(request, question_id):
      question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 """
+
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
